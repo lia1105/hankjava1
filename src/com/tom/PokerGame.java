d06d77e4379f84976cb2d001fa2e901abe3df457
Liya Lin <liya881105@gmail.com>
2018-11-27 12:02:40 +0800

    11/27

--- src/com/tom/PokerGame.java
+++ src/com/tom/PokerGame.java
@@ -8,28 +8,44 @@ public class PokerGame {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		int[] flowers = {0x2660, 0x2665, 0x2666, 0x2663};
+		int[] nums = new int[5];
+		nums[0] =77;
+		nums[4] =98;
+		System.out.println(nums[4]);
+		System.out.println(nums.length);
+		for (int i=0; i<nums.length; i++) {
+			System.out.println(nums[i]);
+			
+		}
+		char f = 0x2665;
+		System.out.println(f);
+		
+		
+		
+		
 		Random random = new Random();
 		int pokerNumber = random.nextInt(13)+1;
-		int poker = random.nextInt(4);
-		
-		char suit =0;
-		switch (poker) {
+		int flower = random.nextInt(4);
+		System.out.print(pokerNumber);
+		char c =0;
+		switch (flower) {
 		case 0:
-			suit='C';
+			c='C';
 			break;
 		case 1:
-			suit='D';
+			c='D';
 			break;
 		case 2:
-			suit='H';
+			c='H';
 			break;
 		case 3:
-			suit='S';
+			c='S';
 			break;
 		
 		}
 		
-		System.out.println(pokerNumber+suit);
+		System.out.println(c);
 	}
 
 }
